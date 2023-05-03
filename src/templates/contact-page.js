8b4b0b76ba832b35ec46a21e372250005500f8c5
kevzy <55018689+kevzy@users.noreply.github.com>
2023-05-03 17:39:03 +0300

    n

--- src/templates/contact-page.js
+++ src/templates/contact-page.js
@@ -1,112 +1,106 @@
 /** @jsx jsx */
 import { jsx } from "theme-ui"
-import { graphql } from "gatsby"
-import { RiSendPlane2Line } from "react-icons/ri"
+import { graphql, Link } from "gatsby"
+import { GatsbyImage } from "gatsby-plugin-image"
+import { RiArrowRightSLine } from "react-icons/ri"
+import {
+  RiFacebookBoxFill,
+  RiTwitterFill,
+  RiLinkedinBoxFill,
+  RiYoutubeFill,
+  RiInstagramFill,
+  RiRssFill,
+  RiGithubFill,
+  RiTelegramFill,
+  RiPinterestFill,
+  RiSnapchatFill,
+  RiSkypeFill,
+  RiWhatsappFill,
+  RiMediumFill,
+  RiBehanceFill,
+} from "react-icons/ri"
+import { faTiktok, FaWordpress, FaVk } from "react-icons/fa"
 
 import Layout from "../components/layout"
+import BlogListHome from "../components/blog-list-home"
 import Seo from "../components/seo"
+import Icons from "../util/socialmedia.json"
 
 export const pageQuery = graphql`
-  query ContactQuery($id: String!) {
+  query HomeQuery($id: String!) {
     markdownRemark(id: { eq: $id }) {
       id
       html
-      excerpt(pruneLength: 140)
       frontmatter {
         title
+        tagline
+        featuredImage {
+          childImageSharp {
+            gatsbyImageData(layout: CONSTRAINED, width: 585, height: 439)
+          }
+        }
+        cta {
+          ctaText
+          ctaLink
+        }
       }
     }
-    site {
-      siteMetadata {
-        title
+    posts: allMarkdownRemark(
+      sort: { order: DESC, fields: [frontmatter___date] }
+      filter: { frontmatter: { template: { eq: "blog-post" } } }
+      limit: 6
+    ) {
+      edges {
+        node {
+          id
+          excerpt(pruneLength: 250)
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            slug
+            title
+            featuredImage {
+              childImageSharp {
+                gatsbyImageData(layout: CONSTRAINED, width: 345, height: 260)
+              }
+            }
+          }
+        }
       }
     }
   }
 `
 
-const Contact = ({ data }) => {
-  const { markdownRemark, site } = data // data.markdownRemark holds your post data
+const HomePage = ({ data }) => {
+  const { markdownRemark, posts } = data // data.markdownRemark holds your post data
   const { frontmatter, html } = markdownRemark
-
-  return (
-    <Layout className="contact-page" sx={contactStyles.contactPage}>
-      <Seo
-        title={frontmatter.title}
-        description={frontmatter.title + " " + site.siteMetadata.title}
-      />
-      <div className="wrapper">
-        <h1>{frontmatter.title}</h1>
-        <div
-          className="description"
-          dangerouslySetInnerHTML={{ __html: html }}
-        />
-        <form
-          className="contact-form"
-          action="/thanks"
-          name="contact"
-          method="POST"
-          data-netlify="true"
-          data-netlify-honeypot="bot-field"
-        >
-          <input type="hidden" name="form-name" value="contact" />
-          <p>
-            <label>
-              Name
-              <input type="text" name="name" required />
-            </label>
-          </p>
-          <p>
-            <label>
-              Email
-              <input type="email" name="email" required />
-            </label>
-          </p>
-          <p>
-            <label>
-              Subject
-              <input type="text" name="subject" required />
-            </label>
-          </p>
-          <p>
-            <label>
-              Message<textarea name="message" required></textarea>
-            </label>
-          </p>
-          <p className="text-align-right">
-            <button
-              className="button"
-              sx={{
-                variant: "variants.button",
-              }}
-              type="submit"
-            >
-              Send Message{" "}
-              <span className="icon -right">
-                <RiSendPlane2Line />
-              </span>
-            </button>
-          </p>
-        </form>
+  const Image = frontmatter.featuredImage
+    ? frontmatter.featuredImage.childImageSharp.gatsbyImageData
+    : ""
+  const sIcons = Icons.socialIcons.map((icons, index) => {
+    return (
+      <div key={"social icons" + index}>
+        {icons.icon === "facebook" ? (
+          <a href={icons.url} target="_blank" aria-label="link to Facebook" rel="noopener noreferrer">
+            <RiFacebookBoxFill alt="Facebook icon"/>
+          </a>
+        ) : (
+          ""
+        )}
+        {icons.icon === "instagram" ? (
+          <a href={icons.url} target="_blank" aria-label="link to Instagram" rel="noopener noreferrer">
+            <RiInstagramFill alt="Instagram icon" />
+          </a>
+        ) : (
+          ""
+        )}
+        {icons.icon === "whatsapp" ? (
+          <a href={icons.url} target="_blank" aria-label="link to Dribbble" rel="noopener noreferrer">
+            <RiWhatsappFill alt="Dribbble icon" />
+          </a>
+        ) : (
+          ""
+        )}
       </div>
-    </Layout>
-  )
-}
-
-export default Contact
-
-const contactStyles = {
-  contactPage: {
-    input: {
-      border: "6px solid",
-      borderColor: "inputBorder",
-      bg: "inputBackground",
-      outline: "none",
-    },
-    textarea: {
-      border: "6px solid",
-      borderColor: "inputBorder",
-      bg: "inputBackground",
-      outline: "none",
-    },
-  },
+    )
+  })
 }
